2f98ba90e95613e627d43028b3802216d03f076b
Nayan-Mudewar <nayanmudewar2002@gmail.com>
2025-04-18 11:38:30 +0530

    Add User and Movie model classes

---
+++ models/movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self,movie_id, title, duration,language):
+        self.movie_id=movie_id
+        self.title=title
+        self.duration=duration
+        self.language=language
+        
